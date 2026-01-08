72b49acf3287b9cab07b6af696691c4077a530d6
Pkto01 <theosuk01@gmail.com>
2026-01-08 14:46:30 +0100

    scalingo ready

--- main.go
+++ main.go
@@ -1,13 +1,23 @@
 package main
 
 import (
+	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
-	// Sert les fichiers du dossier "static"
-	http.Handle("/", http.FileServer(http.Dir("static")))
+	// Récupération du port donné par Scalingo
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080" // fallback local
+	}
 
-	// Lance le serveur sur http://localhost:8080
-	http.ListenAndServe(":8080", nil)
+	// Sert les fichiers du dossier "static"
+	fs := http.FileServer(http.Dir("static"))
+	http.Handle("/", fs)
+
+	log.Println("Server started on port", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
+	
